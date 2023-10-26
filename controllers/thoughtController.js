7bcd7e972fd4d8a63bfc6303fc9dc737ecae71ba
Flagg217 <flagg217@gmail.com>
2023-10-26 04:04:13 -0700

    worked on createReaction function

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -43,29 +43,34 @@ module.exports = {
     } catch (err) {
       res.status(500).json(err);
     }
-  }
+  },
   // Update a thought
   updateThought(req, res) {
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
       { $set: req.body },
-      { runValidators: true, new: true }
+      { new: true }
     )
-      .then((thought) =>
-        !thought
-          ? res.status(404).json({ message: 'No thought with this id!' })
-          : res.json(thought)
-      )
+      .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
 
-  createReaction(req, res) {
-    Thought.create(req.body)
-      .then((thought) => res.json(thought))
-      .catch((err) => {
-        console.log(err);
-        return res.status(500).json(err);
+  async createReaction(req, res) {
+    try {
+      const addReaction = await Thought.findOneAndUpdate({
+        _id: req.params.thoughtId,
       });
+      if (addReaction) {
+        await Thought.findOneAndUpdate(
+          { _id: req.params.thoughtId },
+          { $addToSet: { reactions: req.body } },
+          { new: true }
+        );
+      }
+      res.json("Reaction added!");
+    } catch (err) {
+      res.status(500).json(err);
+    }
   },
   // Delete a thought
   deleteReaction(req, res) {
